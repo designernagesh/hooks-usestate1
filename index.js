cf9e029a19bc45130476db52aacc4513f9e47267
designernagesh <designer.nagesh@gmail.com>
2020-02-15 13:23:13 +0530

    1 file modified

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ import './style.css';
 function App(){
   const [ count, setCount ] = useState(0)
 
+  const updateCount = (e) => {
+    setCount(e.target.value)
+    console.log(count)
+  }
+  const incrementCount = () => {
+    setCount(count + 1)
+  }
   return (
     <div>      
       <h2>
-        Start editing to see some magic happen :)
+        Hooks ( useState ) Example 1
       </h2>
+      <input value={count} onChange={updateCount} />
+      <br /><br />
+      <button onClick={incrementCount}>Increase Count Value</button>
     </div>
   );
 }
